9a7c4b309c2e6af9b6aa507591b2704bd4d50364
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-18 16:41:54 -0400

    Base display number

---
+++ main.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+}
+
+
+var $display = document.querySelector('display-number');
+
+//listen for number press
+//execute some code when a number is pressed
+//create a variable to store the pressed number
+//update the page to display the the pressed number
+
+
+
+function numberButtonPressed(event) {
+  var number = Number (event.target.textContent);
+  elDisplayNumber =
+}
+
+
+
+
+
+
+[].forEach.call(.document.querySelectorAll(".keypad-button.number") function (element) {
+  element.addEventListener('click', numberButtonPressed);
+}, false);
+
+
+
+})();
